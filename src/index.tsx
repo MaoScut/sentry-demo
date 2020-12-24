a4a9df0bf2ff77deed9b7a62599f985a9024359b
madp <849345372@qq.com>
2020-12-24 23:05:01 +0800

    trigger err

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as Sentry from "@sentry/react";
 
+Sentry.init({ dsn: "https://85401bc9cdd947759bd432703f335207@o495959.ingest.sentry.io/5569567" });
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// Can also use with React Concurrent Mode
+// ReactDOM.createRoot(document.getElementById('root')).render(<App />);
 ReactDOM.render(
   <React.StrictMode>
     <App />
